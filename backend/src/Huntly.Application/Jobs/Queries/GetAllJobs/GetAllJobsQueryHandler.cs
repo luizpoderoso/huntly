378eb4d6943ee795e6c49dcad6f73fec6c1ad8c9
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-12 18:43:25 -0300

    feat(app): add GetAllJobsQueryHandler

---
+++ backend/src/Huntly.Application/Jobs/Queries/GetAllJobs/GetAllJobsQueryHandler.cs
@@ -0,0 +1,26 @@
+using Huntly.Application.Shared.Interfaces;
+using Huntly.Core.Job.Repositories;
+using MediatR;
+
+namespace Huntly.Application.Jobs.Queries.GetAllJobs;
+
+public class GetAllJobsQueryHandler(
+    IJobApplicationRepository repository,
+    IUserContext userContext)
+    : IRequestHandler<GetAllJobsQuery, IReadOnlyCollection<JobSummaryDto>>
+{
+    public async Task<IReadOnlyCollection<JobSummaryDto>> Handle(GetAllJobsQuery request, CancellationToken ct)
+    {
+        var jobs = await repository.GetAllByUserIdAsync(userContext.UserId, ct);
+
+        return jobs.Select(j => new JobSummaryDto(
+            j.Id,
+            j.CompanyName.Value,
+            j.Position.Value,
+            j.Status.ToString(),
+            j.JobUrl?.Value,
+            j.CreatedAt,
+            j.UpdatedAt
+            )).ToList().AsReadOnly();
+    }
+}
