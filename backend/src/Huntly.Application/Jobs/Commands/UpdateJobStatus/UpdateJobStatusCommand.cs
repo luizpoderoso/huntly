3a36912d0f437b0486425baf311dd4b9e2696d6f
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-12 23:26:35 -0300

    feat(app): add UpdateJobStatusCommand

---
+++ backend/src/Huntly.Application/Jobs/Commands/UpdateJobStatus/UpdateJobStatusCommand.cs
@@ -0,0 +1,9 @@
+using Huntly.Core.Job.Enums;
+using MediatR;
+
+namespace Huntly.Application.Jobs.Commands.UpdateJobStatus;
+
+public record UpdateJobStatusCommand(
+    Guid JobId,
+    ApplicationStatus NewStatus
+) : IRequest<Guid>;
