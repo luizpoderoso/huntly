b1d5c05fa82716ccdc45514842e44c2f333f3d58
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-13 15:44:32 -0300

    feat(infra): add NoteConfiguration

---
+++ backend/src/Huntly.Infra/Persistence/Context/Configurations/Jobs/NoteConfiguration.cs
@@ -0,0 +1,14 @@
+using Huntly.Core.Job.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Huntly.Infra.Persistence.Context.Configurations.Jobs;
+
+public class NoteConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.Property(n => n.Content)
+            .IsRequired();
+    }
+}
