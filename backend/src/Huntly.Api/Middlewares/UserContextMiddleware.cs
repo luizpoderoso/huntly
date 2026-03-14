ce622bf497df05de9fe985e47df6d669d1277baa
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-14 02:41:58 -0300

    feat(api): add UserContextMiddleware

---
+++ backend/src/Huntly.Api/Middlewares/UserContextMiddleware.cs
@@ -0,0 +1,28 @@
+using System.Security.Claims;
+using Huntly.Application.Shared.Interfaces;
+using Huntly.Infra.Security;
+
+namespace Huntly.Api.Middlewares;
+
+public class UserContextMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context, IUserContext userContext)
+    {
+        if (context.User.Identity?.IsAuthenticated == true)
+        {
+            var concreteContext = (UserContext)userContext;
+
+            var userIdClaim = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (Guid.TryParse(userIdClaim, out var userId))
+            {
+                concreteContext.UserId = userId;
+                concreteContext.IsAuthenticated = true;
+            }
+
+            concreteContext.Username = context.User.FindFirst(ClaimTypes.Name)?.Value
+                                       ?? string.Empty;
+        }
+
+        await next(context);
+    }
+}
