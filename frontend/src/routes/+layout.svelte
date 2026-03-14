b87362814b48911970a95e2fda0f65a0cb7d8c56
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-14 18:50:03 -0300

    refactor(front): clear layout and page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import type { Snippet } from 'svelte';
+    import '../app.css';
 
-	let { children } = $props();
+    let { children }: { children: Snippet } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
