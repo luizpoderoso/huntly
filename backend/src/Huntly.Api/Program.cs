81213ad17e0834b5bf96da964db6a0cf13dbb4c3
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-14 02:44:38 -0300

    feat(api): add API configurations at Program.cs

--- backend/src/Huntly.Api/Program.cs
+++ backend/src/Huntly.Api/Program.cs
@@ -1,6 +1,31 @@
+using FastEndpoints;
+using FastEndpoints.Security;
+using Huntly.Api.Middlewares;
+using Huntly.Application;
+using Huntly.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddAuthenticationJwtBearer(s => s.SigningKey = builder.Configuration["Jwt:Secret"]!)
+    .AddAuthorization()
+    .AddFastEndpoints()
+    .AddCors(options =>
+        options.AddDefaultPolicy(p =>
+            p.WithOrigins(builder.Configuration["Cors:Origin"]!)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseAuthentication();
+app.UseMiddleware<UserContextMiddleware>();
+app.UseAuthorization();
+app.UseCors();
+app.UseFastEndpoints();
 
 app.Run();
