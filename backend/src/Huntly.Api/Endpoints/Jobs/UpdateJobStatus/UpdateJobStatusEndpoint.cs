e194902cb4db70ab6db825f149a356c056dcd71c
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-14 16:33:30 -0300

    feat(api): add UpdateJobStatusEndpoint

---
+++ backend/src/Huntly.Api/Endpoints/Jobs/UpdateJobStatus/UpdateJobStatusEndpoint.cs
@@ -0,0 +1,22 @@
+using FastEndpoints;
+using Huntly.Application.Jobs.Commands.UpdateJobStatus;
+using MediatR;
+
+namespace Huntly.Api.Endpoints.Jobs.UpdateJobStatus;
+
+public class UpdateJobStatusEndpoint(IMediator mediator) : Endpoint<UpdateJobStatusRequest>
+{
+    public override void Configure()
+    {
+        Patch("/{id:guid}/status");
+        Group<JobsGroup>();
+    }
+    
+    public override async Task HandleAsync(UpdateJobStatusRequest req, CancellationToken ct)
+    {
+        var id = Route<Guid>("id");
+        var command = new UpdateJobStatusCommand(id, req.NewStatus);
+        await mediator.Send(command, ct);
+        await Send.NoContentAsync(ct);
+    }
+}
