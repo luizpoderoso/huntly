1e6769eb3e6123d4aed2fca53e5d199b9df87c61
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-15 14:39:05 -0300

    feat(app): add AddInterviewCommandValidator

---
+++ backend/src/Huntly.Application/Jobs/Commands/AddInterview/AddInterviewCommandValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+
+namespace Huntly.Application.Jobs.Commands.AddInterview;
+
+public class AddInterviewCommandValidator : AbstractValidator<AddInterviewCommand>
+{
+    public AddInterviewCommandValidator()
+    {
+        RuleFor(x => x.JobApplicationId)
+            .NotEmpty().WithMessage("Job application ID is required.");
+        
+        RuleFor(x => x.InterviewType)
+            .IsInEnum().WithMessage("Interview type is required.");
+        
+        RuleFor(x => x.ScheduledAt)
+            .NotEmpty().WithMessage("Interview schedule is required.")
+            .GreaterThan(DateTime.UtcNow).WithMessage("Interview must be scheduled in the future.");;
+    }
+}
