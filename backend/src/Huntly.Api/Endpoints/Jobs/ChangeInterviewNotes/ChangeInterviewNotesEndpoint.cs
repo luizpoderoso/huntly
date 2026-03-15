1b85bb6cbb173eb8f1fd25d03e0daf0fb0d56d0a
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-15 18:53:29 -0300

    feat(api): add ChangeInterviewNotesEndpoint

---
+++ backend/src/Huntly.Api/Endpoints/Jobs/ChangeInterviewNotes/ChangeInterviewNotesEndpoint.cs
@@ -0,0 +1,23 @@
+using FastEndpoints;
+using Huntly.Application.Jobs.Commands.ChangeInterviewNotes;
+using MediatR;
+
+namespace Huntly.Api.Endpoints.Jobs.ChangeInterviewNotes;
+
+public class ChangeInterviewNotesEndpoint(IMediator mediator) : Endpoint<ChangeInterviewNotesRequest>
+{
+    public override void Configure()
+    {
+        Patch("{jobId:guid}/interviews/{interviewId:guid}/notes");
+        Group<JobsGroup>();
+    }
+
+    public override async Task HandleAsync(ChangeInterviewNotesRequest req, CancellationToken ct)
+    {
+        var jobId = Route<Guid>("jobId");
+        var interviewId = Route<Guid>("interviewId");
+        var command = new ChangeInterviewNotesCommand(jobId, interviewId, req.NewInterviewNotes);
+        await mediator.Send(command, ct);
+        await Send.NoContentAsync(ct);
+    }
+}
