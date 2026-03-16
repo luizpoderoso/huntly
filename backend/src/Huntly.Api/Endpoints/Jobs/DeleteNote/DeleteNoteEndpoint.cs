c818222a179e27836752bcbdc3f87dc91370fade
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-16 00:08:09 -0300

    feat(api): add DeleteNoteEndpoint

---
+++ backend/src/Huntly.Api/Endpoints/Jobs/DeleteNote/DeleteNoteEndpoint.cs
@@ -0,0 +1,23 @@
+using FastEndpoints;
+using Huntly.Application.Jobs.Commands.DeleteNote;
+using MediatR;
+
+namespace Huntly.Api.Endpoints.Jobs.DeleteNote;
+
+public class DeleteNoteEndpoint(IMediator mediator) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Delete("{jobId:guid}/notes/{noteId:guid}");
+        Group<JobsGroup>();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var jobId = Route<Guid>("jobId");
+        var noteId = Route<Guid>("noteId");
+        var command = new DeleteNoteCommand(jobId, noteId);
+        await mediator.Send(command, ct);
+        await Send.NoContentAsync(ct);
+    }
+}
