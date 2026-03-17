8878dcd844860c7d9f747824cbd0c642b616f40b
Luiz Poderoso <luizpoderoso72@outlook.com>
2026-03-17 03:48:18 -0300

    docs: add scalar document at '/api/scalar'

--- backend/src/Huntly.Api/Program.cs
+++ backend/src/Huntly.Api/Program.cs
@@ -1,9 +1,11 @@
 using System.Text.Json.Serialization;
 using FastEndpoints;
 using FastEndpoints.Security;
+using FastEndpoints.Swagger;
 using Huntly.Api.Middleware;
 using Huntly.Application;
 using Huntly.Infra;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,11 @@ builder.Services
     .AddAuthenticationJwtBearer(s => s.SigningKey = builder.Configuration["Jwt:Secret"]!)
     .AddAuthorization()
     .AddFastEndpoints()
+    .SwaggerDocument(o => o.DocumentSettings = s =>
+    {
+        s.Title = "Huntly API";
+        s.Version = "v1";
+    })
     .AddCors(options =>
         options.AddDefaultPolicy(p =>
             p.WithOrigins(builder.Configuration["Cors:Origin"]!)
@@ -31,6 +38,13 @@ app.UseFastEndpoints(c =>
 {
     c.Endpoints.RoutePrefix = "api";
     c.Serializer.Options.Converters.Add(new JsonStringEnumConverter());
+});
+app.UseSwaggerGen();
+app.MapScalarApiReference(options =>
+{
+    options.WithOpenApiRoutePattern("swagger/v1/swagger.json")
+        .WithTitle("Huntly API Reference")
+        .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
 });
 
 app.Run();
